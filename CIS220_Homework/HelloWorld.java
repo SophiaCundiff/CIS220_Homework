f3b3c021d64c1c0afed293dc064f8180e3d63179
SophiaCundiff <sophiabcundiff@gmail.com>
2024-01-26 14:30:12 -0700

    Hello world!

--- CIS220_Homework/HelloWorld.java
+++ CIS220_Homework/HelloWorld.java
@@ -1,8 +1,9 @@
+// Sophia Cundiff
+
 import java.util.Scanner;
 
 public class HelloWorld {
-
-	public static void main() {
-	System.out.println("Hello World!!");
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
 	}
 }
